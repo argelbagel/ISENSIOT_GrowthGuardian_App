6faf1b440ba359e55bb86b099a6e941d3b6a1b4b
Hugo <s1117785@student.hsleiden.nl>
2023-11-30 15:41:27 +0100

    Add plant pagina toegevoegd

--- growth_guardian/lib/views/addPage.dart
+++ growth_guardian/lib/views/addPage.dart
@@ -10,6 +10,141 @@ class AddPage extends StatefulWidget {
 class _AddPageState extends State<AddPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Container(
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Container(
+                width: screenWidth * 0.9,
+                height: screenWidth * 0.9,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "Wetenschappelijke naam gedetecteerde plant",
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          /*
+          Padding(
+            padding: const EdgeInsets.only(left: 40.0),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "Naam",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),*/
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+            child: Container(
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.surface,
+                  labelText: 'Geef de plant een herkenbare naam',
+                ),
+              ),
+            ),
+          ),
+          /*Padding(
+            padding: const EdgeInsets.only(left: 40.0, top: 10.0),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "Locatie",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),*/
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+            child: Container(
+              height: 50.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.surface,
+                  labelText: 'Waar staat de plant in het huis?',
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "Ideale leefomgeving voor deze plant:\nTemperatuur:              ...\nLuchtvochtigheid:      ...\nLichtniveau:                ...",
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Container(
+                  width: 340.0,
+                  height: 50.0,
+                  child: ElevatedButton(
+                    child: Text(
+                      "Add plant",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      elevation: 0,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
